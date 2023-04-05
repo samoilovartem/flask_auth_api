dd80157bc227b88a6f01abebba3fd71d2dfe1c4e
Artem Samoilov <samoylovartem07@gmail.com>
2023-04-05 19:16:50 +0800

    * Fixes async

--- auth_api/migrations/env.py
+++ auth_api/migrations/env.py
@@ -5,7 +5,7 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
-from src.db.databases import db
+from src.db.sql import db
 
 config = context.config
 
