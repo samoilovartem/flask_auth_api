2fa0b3e1239d217b99ee31a5c0883416381dfd4a
Sergei Timoshenkov <serkoe2@gmail.com>
2023-04-11 15:16:10 +0300

    Добавил формирвоание документации при старте приложения

--- auth_api/src/core/documentation.py
+++ auth_api/src/core/documentation.py
@@ -6,6 +6,27 @@ def load_spec(app: Flask, spec: APISpec):
     """Load routes, define schemes"""
 
     with app.app_context():
+
+        # for _ in app.url_map.iter_rules():
+        #     if _.endpoint in ('static', 'security.static'):
+        #         continue
+        #     print(_)
+        #     print(dir(_))
+        #
+        #     # тоже чт и fn_name
+        #     print(_.endpoint)
+        #     # Rule bind c GET
+        #     print(dir(_.build))
+        #     # Аргументы функции
+        #     print(_.build.__func__.__annotations__)
+        #     # Докстринга функции
+        #     print(_.build.__func__.__doc__)
+        #     # Методы Доступные, 1й GET
+        #     print(_.methods)
+        #     # Ссылка от хоста
+        #     print(_.rule)
+        #     break
+
         for fn_name in app.view_functions:
             if fn_name in ('static', 'security.static'):
                 continue
